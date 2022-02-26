589c068ce8ae8dea37c31d4cab6ec30ce084cdec
andrejancevski <andrej.ancevski9@gmail.com>
2022-02-26 21:42:18 +0100

    [FS - CH] - Added inital setup for connecting with casssandra by using the dataStax astra service which enables having cassandra instances on the cloud. Added the appropriabte application properties so the backend can connect with the database

--- be-chat-application/src/main/java/com/fs/chat/application/chatapplication/ChatApplication.java
+++ be-chat-application/src/main/java/com/fs/chat/application/chatapplication/ChatApplication.java
@@ -1,13 +1,26 @@
 package com.fs.chat.application.chatapplication;
 
+import com.fs.chat.application.chatapplication.configuration.DataStaxAstraProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import java.nio.file.Path;
 
 @SpringBootApplication
+@EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class ChatApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChatApplication.class, args);
+	}
+
+	@Bean
+	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
+		Path bundle = astraProperties.getSecureConnectBundle().toPath();
+		return builder -> builder.withCloudSecureConnectBundle(bundle);
 	}
 
 }
