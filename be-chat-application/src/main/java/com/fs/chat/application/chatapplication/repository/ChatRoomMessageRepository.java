46ac605ba7fa1536099e92e6b219ebb1ec543ef6
andrejancevski <andrej.ancevski9@gmail.com>
2022-02-27 13:42:44 +0100

    [FS - CH] - Annotated repository with suitable annotation

--- be-chat-application/src/main/java/com/fs/chat/application/chatapplication/repository/ChatRoomMessageRepository.java
+++ be-chat-application/src/main/java/com/fs/chat/application/chatapplication/repository/ChatRoomMessageRepository.java
@@ -2,6 +2,8 @@ package com.fs.chat.application.chatapplication.repository;
 
 import com.fs.chat.application.chatapplication.models.ChatRoomMessage;
 import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ChatRoomMessageRepository extends CassandraRepository<ChatRoomMessage, Long> {
 }
--- be-chat-application/src/main/java/com/fs/chat/application/chatapplication/repository/UserRepository.java
+++ be-chat-application/src/main/java/com/fs/chat/application/chatapplication/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.fs.chat.application.chatapplication.repository;
 
 import com.fs.chat.application.chatapplication.models.User;
 import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends CassandraRepository<User, String> {
     Optional<User> findByUserName(String userName);
 }
